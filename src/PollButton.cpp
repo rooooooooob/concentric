0ce638ea4f1cf7fc62737260a12fbc7b1ee7f918
rooooooooob <raftias@gmail.com>
2013-11-21 00:25:52 -0800

    allowed the user to configure which axes to use for aiming in the menu in addition to the actions previously there

--- src/PollButton.cpp
+++ src/PollButton.cpp
@@ -22,26 +22,61 @@ void PollButton::update ()
     if (polling)
 	{
 		std::string actions [5] = { "jump", "move_right", "move_left", "swing", "throw"};
-		label = actions[pollPos];
+		
 		unsigned int joyID;
 		if (input.findController(joyID))
 		{
 			config.controller.setJoystickID(joyID);
 		}
-		je::Controller::Bind b(config.controller.getLastInputAsBind());
-		if (b.device != je::Controller::Bind::Device::Invalid)
+		
+		if (pollPos < 5)
 		{
-			config.controller.setKeybinds(actions[pollPos], {b});
-			//std::cout << actions[pollPos] << ": " << b.key << "\n";
-			std::cout << pollPos << "\n";
-
-			pollPos++;
-			if (pollPos == 5)
+			label = actions[pollPos];
+			je::Controller::Bind b(config.controller.getLastInputAsBind());
+			if (b.device != je::Controller::Bind::Device::Invalid)
 			{
-				pollPos = 0;
-				polling = false;
-				label = "P1 Controls";
+				config.controller.setKeybinds(actions[pollPos], {b});
+				//std::cout << actions[pollPos] << ": " << b.key << "\n";
+				std::cout << pollPos << "\n";
+
+				++pollPos;
 			}
+		}
+		else if (pollPos == 5)
+		{
+			label = "Aim right (left click for mouse aim)";
+			je::Controller::AxisBind bind(config.controller.getLastAxisMovementAsBind());
+			if (bind.device != je::Controller::AxisBind::Device::Invalid)
+			{
+				config.controller.setAxis("aim_x", bind);
+				++pollPos;
+			}
+			else if (input.isButtonPressed(sf::Mouse::Button::Left))
+			{
+				config.controller.setAxis("aim_x", je::Controller::AxisBind(je::Controller::AxisBind::MouseAxis::X, false, je::Controller::AxisBind::Interval(-128, 128), nullptr));
+				++pollPos;
+			}
+		}
+		else if (pollPos == 6)
+		{
+			label = "Aim Down (left click for mouse aim)";
+			je::Controller::AxisBind bind(config.controller.getLastAxisMovementAsBind());
+			if (bind.device != je::Controller::AxisBind::Device::Invalid)
+			{
+				config.controller.setAxis("aim_y", bind);
+				++pollPos;
+			}
+			else if (input.isButtonPressed(sf::Mouse::Button::Left))
+			{
+				config.controller.setAxis("aim_y", je::Controller::AxisBind(je::Controller::AxisBind::MouseAxis::Y, false, je::Controller::AxisBind::Interval(-128, 128), nullptr));
+				++pollPos;
+			}
+		}
+		else
+		{
+			pollPos = 0;
+			polling = false;
+			label = "P1 Controls";
 		}
 	}
 }
--- src/Window.cpp
+++ src/Window.cpp
@@ -4,49 +4,46 @@ namespace con
 {
 
 Window::Window () :
-    frame (sf::Vector2f (0, 0)),
-    open (false)
+	frame (sf::Vector2f (0, 0)),
+	open (false)
 {
-    frame.setPosition (0, 0);
-    frame.setFillColor (sf::Color::Black);
+	frame.setPosition (0, 0);
+	frame.setFillColor (sf::Color::Black);
 }
 
 Window::Window (const sf::Vector2f &pos, const sf::Vector2f &dimensions, je::Level *level, sf::Color color) :
-    frame (dimensions),
-    open (false)
+	frame (dimensions),
+	open (false)
 {
-    frame.setPosition (pos);
-    frame.setFillColor (color);
+	frame.setPosition (pos);
+	frame.setFillColor (color);
 }
 
 void Window::draw (sf::RenderTarget& target, const sf::RenderStates &states) const
 {
-    target.draw (frame, states);
-    for (auto& b : buttons)
-    {
-        b->draw (target, states);
-    }
+	target.draw (frame, states);
+	for (auto& b : buttons)
+	{
+		b->draw (target, states);
+	}
 }
 
 void Window::update ()
 {
-    for (auto& b : buttons)
-    {
-        b->update();
-    }
+	for (auto& b : buttons)
+	{
+		b->update();
+	}
 }
 
 bool Window::isOpen () const
 {
-    return open;
+	return open;
 }
 
 void Window::toggle ()
 {
-    if (open)
-        open = false;
-    else
-        open = true;
+	open = !open;
 }
 
 }
